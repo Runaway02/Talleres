f96840f62dcdbd0e7bb57029c6481743e155d655
Runaway02 <jordan.idrobo00@usc.edu.co>
2022-09-06 21:57:24 -0500

    Se agrega complemento de la actividad 4

---
+++ Talleres/Taller4y6/app/src/main/java/com/taller4/DisplayMessageActivity.java
@@ -0,0 +1,28 @@
+package com.taller4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import org.w3c.dom.Text;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
