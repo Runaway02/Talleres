2453aaf72a9782acfd3db62b980ab59d0f722a28
Runaway02 <jordan.idrobo00@usc.edu.co>
2022-10-04 22:02:04 -0500

    Taller 7

---
+++ Talleres/Taller7/CrudUsc/src/main/java/com/usc/crud/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.usc.crud.controller;
+
+import com.usc.crud.model.Empleado;
+import com.usc.crud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private  UserService service;
+
+
+    @GetMapping("/consultar/{id}")
+    public ResponseEntity<?> filtrar(@PathVariable Empleado empleado){
+
+        return ResponseEntity.ok(service.finByUser(empleado));
+
+    }
+
+
+    // create employee rest api
+    @PostMapping("/guardar")
+    public ResponseEntity<Empleado> createEmployee(@RequestBody Empleado empleado) {
+        Empleado temporal = service.guardarUser(empleado);
+        try{
+            return ResponseEntity.created(new URI("/guardar"+temporal.getId())).body(temporal);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+    }
+
+    @GetMapping("/consultarAll")
+    public ResponseEntity<?> consultarByUser(){
+
+        return ResponseEntity.ok(service.buscarTdoso()
+        );
+    }
+
+    // update employee rest api
+
+
+
+    // delete employee rest api
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable Long id){
+       String msj = service.eliminarUser(id);
+        return ResponseEntity.ok(msj);
+    }
+
+}
